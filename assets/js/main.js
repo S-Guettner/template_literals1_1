8a98550e27d0c937fb09a8ac77bd0c03fbdfa573
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-24 14:37:18 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const vorname = "Sven"
+const nachname = "Güttner"
+let alter = "29"
+const geburtsort = "Mönchengladbach"
+const größe = "183cm"
+let gewicht = "88kg"
+let hobby = "snowboarden"
+let computerspiele = "cod"
+let lieblingsessen = "chinesisch"
+let lieblingsjahreszeit = "sommer"
+
+
+let text = `Mein Name ist ${vorname} ${nachname} ich bin ${alter} Jahre alt, in ${geburtsort} geboren. Ich bin ${größe} groß und wiege ${gewicht}. Eins meiner hobbys ist ${hobby}, sowie ${computerspiele}. Meine lieblingsjahreszeit ist der ${lieblingsjahreszeit} und mein lieblingsessen ist ${lieblingsessen} `
+console.log(text);
+
+document.write(text)
